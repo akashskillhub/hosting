3e7d25b8b4190261c354603ac9658536c3fc5b60
akash@maticui <akash@maticui.com>
2025-02-21 14:43:54 +0530

    initial setup

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+require("dotenv").config()
+
+const app = express()
+app.use(express.json()) // 👈 stores body to req.body
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+})) // CORS 
+
+//                      call this 👇 if url has http://localhost:5000/api/blog
+app.use("/api/blog", require("./blog.route"))
+app.use("*", (req, res) => {
+    res.status(404).json({ message: "resource not found" })
+})
+
+mongoose.connect(process.env.MONGO_URL)
+// once mongoose connects to mongodb 👇call this function
+mongoose.connection.once("open", () => {
+    console.log("db connected")
+    app.listen(process.env.PORT, console.log("server running"))
+})
